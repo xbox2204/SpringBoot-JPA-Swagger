90935ef1fbe6bb3e79d16f3c5768266a6afc4099
xbox2204 <vineetmisra712@gmail.com>
2020-09-28 00:01:14 +0530

    first commit

---
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,56 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import springfox.documentation.service.Contact;
+
+@EnableWebMvc
+@EnableSwagger2
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+	@Bean
+	public Docket productApi() {
+		Contact contact =new Contact(
+				"Vineet Mishra",
+				"https://github.com/xbox2204",
+				"whatwillyoudo@withmyemail.com"
+		);
+		ApiInfo apiInfo= new ApiInfoBuilder().title("VINEET SPRING-BOOT API")
+				.description("Spring-Boot for all")
+				.termsOfServiceUrl("jUST CHILL!!!")
+				.contact(contact)
+				.licenseUrl("something@something.com").version("1.0").build();
+
+		return new Docket(DocumentationType.SWAGGER_2).apiInfo(apiInfo)
+				.select()
+				.apis(RequestHandlerSelectors.any())
+				.build();
+	}
+
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+
+		registry.addResourceHandler("swagger-ui.html")
+				.addResourceLocations("classpath:/META-INF/resources/");
+
+		registry.addResourceHandler("/webjars/**")
+				.addResourceLocations("classpath:/META-INF/resources/webjars/");
+
+	}
+}
---
+++ src/main/java/com/example/demo/controller/BookController.java
@@ -0,0 +1,65 @@
+package com.example.demo.controller;
+
+import com.example.demo.exception.BookNotFoundException;
+import com.example.demo.model.Book;
+import com.example.demo.repository.BookRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.http.ResponseEntity;
+//import javax.validation.Valid;
+import java.util.List;
+
+@RestController
+@RequestMapping("/vineet")
+public class BookController {
+
+    @Autowired
+    BookRepository bookRepository;
+
+    // Get All Notes
+    @GetMapping("/books")
+    public List<Book> getAllNotes() {
+        return bookRepository.findAll();
+    }
+
+    // Create a new Note
+    @PostMapping("/books")
+    public Book createNote(@RequestBody Book book) {
+        return bookRepository.save(book);
+    }
+
+    // Get a Single Note
+    @GetMapping("/books/{id}")
+    public Book getNoteById(@PathVariable(value = "id") Long bookId) throws BookNotFoundException {
+        return bookRepository.findById(bookId)
+                .orElseThrow(() -> new BookNotFoundException(bookId));
+    }
+
+    // Update a Note
+    @PutMapping("/books/{id}")
+    public Book updateNote(@PathVariable(value = "id") Long bookId,
+                           @RequestBody Book bookDetails) throws BookNotFoundException {
+
+        Book book = bookRepository.findById(bookId)
+                .orElseThrow(() -> new BookNotFoundException(bookId));
+
+        book.setBook_name(bookDetails.getBook_name());
+        book.setAuthor_name(bookDetails.getAuthor_name());
+        book.setIsbn(bookDetails.getIsbn());
+
+        Book updatedBook = bookRepository.save(book);
+
+        return updatedBook;
+    }
+
+    // Delete a Note
+    @DeleteMapping("/books/{id}")
+    public ResponseEntity<?> deleteBook(@PathVariable(value = "id") Long bookId) throws BookNotFoundException {
+        Book book = bookRepository.findById(bookId)
+                .orElseThrow(() -> new BookNotFoundException(bookId));
+
+        bookRepository.delete(book);
+
+        return ResponseEntity.ok().build();
+    }
+}
